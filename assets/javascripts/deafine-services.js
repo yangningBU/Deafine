8e9d8ab1f818c05567f4106922c579f3e9b792bc
Yonatan Laurence <yangning@bu.edu>
2014-05-25 20:36:25 -0400

    New Carousel / Modal Implementation

---
+++ assets/javascripts/deafine-services.js
@@ -0,0 +1,4 @@
+services = angular.module('Deafine.services', []);
+services.factory('lectures',function() {
+	return [{gloss:"conduct experiment",path:"Cond Experiment lecture_CB BS",preview:""}];
+	});
